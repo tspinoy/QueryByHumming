ee5d002fb2aaa8d9f9d688d0844f2172b6bf43c0
thijsspinoy <thijs.spinoy@gmail.com>
2017-02-24 12:17:10 +0100

    Python server opbouwen om functies van een python bestand te kunnen oproepen bij button-click.

--- Main.py
+++
@@ -1,4 +0,0 @@
-import webbrowser
-import os
-
-webbrowser.open("file://" + os.path.realpath("Home.html"))
--- match.py
+++ match.py
@@ -1,4 +1,7 @@
+import json
+
 def longestCommonSubstring(query, goal):
     return query + ", len = " + str(len(query))
+
 
 #print(longestCommonSubstring("Hello World", "nyi"))
