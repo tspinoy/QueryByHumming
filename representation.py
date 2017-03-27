de2e2fdb1c8e94a6d0ff872b3c23d6c4f6cf9478
thijsspinoy <thijs.spinoy@gmail.com>
2017-03-27 11:59:00 +0200

    Onset, IOI en logIOI, librosa als basis voor omzetting naar MIDI + unittests

--- representation.py
+++ representation.py
@@ -1,67 +1,101 @@
-from aubio import source, sink, digital_filter
-from os.path import basename, splitext
+import unittest
 
-def apply_filter(path):
+import librosa
+import numpy as np
 
-    # open input file, get its samplerate
-    s = source(path)
-    samplerate = s.samplerate
+#y, sr = librosa.load(librosa.util.example_audio_file())
+#onset_frames = librosa.onset.onset_detect(y=y, sr=sr)
+#print onset_frames
+#result = librosa.frames_to_time(onset_frames[:20], sr=sr)
+#print result
 
-    # create an A-weighting filter
-    f = digital_filter(7)
-    f.set_a_weighting(samplerate)
-    # alternatively, apply another filter
 
-    # create output file
-    o = sink("filtered_" + splitext(basename(path))[0] + ".wav", samplerate)
+def load_file(music):
+    """
+    :param music: a music file of any format
+    :return: typle (y, sr) ; y = an array with the audio time series & sr = sample rate
+    """
+    return librosa.load(music)
 
-    total_frames = 0
-    while True:
-        samples, read = s()
-        filtered_samples = f(samples)
-        o(filtered_samples, read)
-        total_frames += read
-        if read < s.hop_size: break
 
-    duration = total_frames / float(samplerate)
-    print ("read {:s}".format(s.uri))
-    print ("applied A-weighting filtered ({:d} Hz)".format(samplerate))
-    print ("wrote {:s} ({:.2f} s)".format(o.uri, duration))
+def get_frequencies(music):
+    y, sr = load_file(music)
+    frequencies = librosa.core.fft_frequencies(sr=sr, n_fft=5)
+    print frequencies
+    return frequencies
 
-#apply_filter("/Users/thijsspinoy/OneDrive/Muziek/2 Unlimited - No Limit.mp3")
 
-import sys
-from aubio import source, onset
+def convert_frequencies_to_midi(frequencies_array):
+    midi = librosa.core.hz_to_midi(frequencies_array)
+    return midi
 
-win_s = 512                 # fft size
-hop_s = win_s // 2          # hop size
 
-if len(sys.argv) < 2:
-    print("Usage: %s <filename> [samplerate]" % sys.argv[0])
-    sys.exit(1)
+def convert_frequencies_to_notes(frequencies_array):
+    midi = librosa.core.hz_to_note(frequencies_array)
+    return midi
 
-filename = sys.argv[1]
 
-samplerate = 0
-if len( sys.argv ) > 2: samplerate = int(sys.argv[2])
+def convert_music_to_midi(music):
+    return convert_frequencies_to_midi(get_frequencies(music))
 
-s = source(filename, samplerate, hop_s)
-samplerate = s.samplerate
+print convert_music_to_midi(librosa.util.example_audio_file())
+#print convert_frequencies_to_notes(get_frequencies(librosa.util.example_audio_file()))
 
-o = onset("default", win_s, hop_s, samplerate)
 
-# list of onsets, in samples
-onsets = []
+def get_onset_times(music):
+    """
+    :param music: a music file.
+    :return: an array with the onset times.
+    """
+    y, sr = load_file(music)
+    onsets = librosa.onset.onset_detect(y=y, sr=sr)
+    return onsets
 
-# total number of frames read
-total_frames = 0
-while True:
-    samples, read = s()
-    if o(samples):
-        print("%f" % o.get_last_s())
-        onsets.append(o.get_last())
-    total_frames += read
-    if read < hop_s:
-        break
 
-print len(onsets)
+def ioi(onsets_array):
+    """
+    :param onsets_array: an array with the onset times.
+    :return: a new array with the onset intervals computed by this function.
+    """
+    res = np.zeros(len(onsets_array)-1)
+
+    for i in range(len(onsets_array)):
+        res[i-1] = onsets_array[i] - onsets_array[i-1]
+
+    return res
+
+
+def log_ioi(onsets_array):
+    """
+    :param onsets_array: an array with the onset times.
+    :return: a new array with the logarithm of the onset intervals computed by this function.
+    """
+    res = np.zeros(len(onsets_array)-1)
+
+    for i in range(len(onsets_array)):
+        res[i-1] = np.log2(onsets_array[i] - onsets_array[i-1])
+
+    return res
+
+#print ioi(result)
+#print log_ioi(result)
+
+#pitches, magnitudes = librosa.piptrack(y=y, sr=sr)
+#print pitches
+#print magnitudes
+
+#print pitches[1]
+
+
+#print(log_ioi([1, 2, 4, 8]))
+#print np.log(1)
+#print np.log(8)
+#print np.log(10)
+
+
+class RepresentationTestCase(unittest.TestCase):
+    def test_ioi(self):
+        self.assertTrue(np.all(ioi([1, 2, 4, 8])) == np.all([1, 2, 4]))
+
+    def test_log_ioi(self):
+        self.assertTrue(np.all(log_ioi([1, 2, 4, 8])) == np.all([0, 1, 2]))
