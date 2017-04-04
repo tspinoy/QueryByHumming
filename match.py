86f9b6e2e34e6f9cd0967d8ad19f5baa38931b61
thijsspinoy <thijs.spinoy@gmail.com>
2017-04-04 17:35:07 +0200

    Nieuw LCSS-algoritme

--- match.py
+++ match.py
@@ -25,49 +25,33 @@ def longest_common_substring(query, goal):
     return res # return the longest common substring
 
 
-# other method: longest common subsequence (tolerance)
-def lcss(X, Y):
-    m = len(X)
-    n = len(Y)
-    # An (m+1) times (n+1) matrix
-    C = [[0] * (n + 1) for _ in range(m + 1)]
-    for i in range(1, m+1):
-        for j in range(1, n+1):
-            if X[i-1] == Y[j-1]:
-                C[i][j] = C[i-1][j-1] + 1
+def lcs(a, b):
+    lengths = [[0 for j in range(len(b) + 1)] for i in range(len(a) + 1)]
+    # row 0 and column 0 are initialized to 0 already
+    for i, x in enumerate(a):
+        for j, y in enumerate(b):
+            if x == y:
+                lengths[i + 1][j + 1] = lengths[i][j] + 1
             else:
-                C[i][j] = max(C[i][j-1], C[i-1][j])
-    return C
-
-
-def backTrack(C, X, Y, i, j):
-    if i == 0 or j == 0:
-        return ""
-    elif X[i-1] == Y[j-1]:
-        return backTrack(C, X, Y, i-1, j-1) + X[i-1]
-    else:
-        if C[i][j-1] > C[i-1][j]:
-            return backTrack(C, X, Y, i, j-1)
+                lengths[i + 1][j + 1] = max(lengths[i + 1][j], lengths[i][j + 1])
+    # read the substring out from the matrix
+    result = ""
+    x, y = len(a), len(b)
+    while x != 0 and y != 0:
+        if lengths[x][y] == lengths[x - 1][y]:
+            x -= 1
+        elif lengths[x][y] == lengths[x][y - 1]:
+            y -= 1
         else:
-            return backTrack(C, X, Y, i-1, j)
-
-
-def backTrackAll(C, X, Y, i, j):
-    if i == 0 or j == 0:
-        return set([""])
-    elif X[i-1] == Y[j-1]:
-        return set([Z + X[i-1] for Z in backTrackAll(C, X, Y, i-1, j-1)])
-    else:
-        R = set()
-        if C[i][j-1] >= C[i-1][j]:
-            R.update(backTrackAll(C, X, Y, i, j-1))
-        if C[i-1][j] >= C[i][j-1]:
-            R.update(backTrackAll(C, X, Y, i-1, j))
-        return R
+            assert a[x - 1] == b[y - 1]
+            result = a[x - 1] + result
+            x -= 1
+            y -= 1
+    res = {'algorithm': "lcss", "resultString": result, 'length': len(result)}
+    return res
 
 #longest_common_substring("aaabbbaaa", "aaababaaa")
-#print backTrack(lcss("aaabbbaaa", "aaababaaa"), "aaabbbaaa", "aaababaaa",len("aaabbbaaa"), len("aaababaaa"))
-
+print lcs("aaabbbaaa", "aaababaaa")
 
 def match():
     return "{\"results\": [ {\"title\": \"Titel 1\", \"listen\": \"Listen 1\"}, {\"title\": \"Titel 2\", \"listen\": \"Listen 2\"}, {\"title\": \"Titel 3\", \"listen\": \"Listen 3\"}, {\"title\": \"Titel 4\", \"listen\": \"Listen 4\"}, {\"title\": \"Titel 5\", \"listen\": \"Listen 5\"}  ] }"
